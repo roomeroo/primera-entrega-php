9b5cf4b2a2e5cb868c9f108af1e1c66c86bd9e74
adrian <roomeroo05@gmail.com>
2025-09-19 16:59:01 +0200

    Arreglado problema en el ejercicio4

--- saludar.php
+++ saludar.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    
-    <body>
-        <?php
-            $hora = empty($_GET["hora"])? 0: $_GET("hora");
-            if(is_numeric($hora) && ($hora <= 24 && hora >= 0)){
-                if($hora >= 6 && $hora <= 12){
-                    echo "buenos dias";
-                }else if($hora >= 13 && $hora <= 20){
-                    echo "buenas tardes";
-                }else{
-                    echo "buenas noches";
-                }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <?php
+        $hora = empty($_GET["hora"]) ? 0 : $_GET["hora"];
+        if (is_numeric($hora) && ($hora <= 24 && $hora >= 0)) {
+            if ($hora >= 6 && $hora <= 12) {
+                echo "buenos días";
+            } else if ($hora >= 13 && $hora <= 20) {
+                echo "buenas tardes";
+            } else {
+                echo "buenas noches";
             }
-        ?>
-        <a href="./index.html">Volver atrás</a>
-    </body>
+        } else {
+            echo "Hora inválida.";
+        }
+    ?>
+    <br>
+    <a href="./index.html">Volver atrás</a>
+</body>
 </html>
